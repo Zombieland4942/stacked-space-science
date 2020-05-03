824b54263f5a80ef447019acec65ec34dd3a481e
mitchellhalliday <zombieland4942@hotmail.com>
2020-05-03 14:23:04 +1000

    - Correct an error that would occur if prod modules didn't have any limitations. (Like creative Mod)

--- data-updates.lua
+++ data-updates.lua
@@ -2,7 +2,10 @@
 -- Add new rocket part to all prod module limitations
 
 for _,mod in pairs(data.raw["module"]) do
+
     if mod.effect.productivity then
-        table.insert(mod.limitation , "stack-rocket-part")
+        if mod.limitation then
+            table.insert(mod.limitation , "stack-rocket-part")
+        end
     end
 end
